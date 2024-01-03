5ba26fe29d1a8470fda50ac3a909a6a133f00999
Maksym Rohachov <romax982@gmail.com>
2024-01-03 22:07:07 +0100

    Update the business-logic

---
+++ src/main/java/one/chapter/TrainingFile.java
@@ -0,0 +1,50 @@
+package one.chapter;
+
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresProblem;
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Instances;
+
+import java.io.FileReader;
+public class TrainingFile {
+
+    public static void main(String[] args) {
+
+        String trainingFileName = "dermatologyTrainingSet.arff";
+        String testingFileName = "dermatologyTestingSet.arff";
+
+        try (FileReader trainingReader = new
+                FileReader(trainingFileName); FileReader testingReader =
+                     new
+                             FileReader(testingFileName)) {
+            Instances trainingInstances = new
+                    Instances(trainingReader);
+            trainingInstances.setClassIndex(
+                    trainingInstances.numAttributes() - 1);
+            Instances testingInstances = new
+                    Instances(testingReader);
+
+            testingInstances.setClassIndex(
+                    testingInstances.numAttributes() - 1);
+
+            MultilayerPerceptron mlp = new
+                    MultilayerPerceptron();
+            mlp.setLearningRate(0.1);
+            mlp.setMomentum(0.2);
+            mlp.setTrainingTime(2000);
+            mlp.setHiddenLayers("3");
+
+            mlp.buildClassifier(trainingInstances);
+            // ...
+
+            Evaluation evaluation = new
+                    Evaluation(trainingInstances);
+            evaluation.evaluateModel(mlp, testingInstances);
+
+            System.out.println(evaluation.toSummaryString());
+
+        } catch (Exception ex) {
+            // Handle exceptions
+        }
+    }
+}
