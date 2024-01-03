c30643a0280389c78e21a10fe9539bddf1e609a6
Maksym Rohachov <romax982@gmail.com>
2024-01-03 04:39:34 +0100

    Upload the logic for the chapter #1

--- src/main/java/one/chapter/App.java
+++ src/main/java/one/chapter/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package one.chapter;
 
 /**
  * Hello world!
--- src/main/java/one/chapter/JsonParser.java
+++ src/main/java/one/chapter/JsonParser.java
@@ -1,4 +1,4 @@
-package org.example;
+package one.chapter;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonToken;
