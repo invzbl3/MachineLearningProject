31e65d91fb5f39c25744aff840bbc57b23550144
Maksym Rohachov <romax982@gmail.com>
2024-01-03 04:12:37 +0100

    Upload basic structure

--- src/main/java/org/example/JsonParser.java
+++ src/main/java/org/example/JsonParser.java
@@ -1,29 +1,49 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonToken;
-
+import java.io.File;
 import java.io.IOException;
 
 public class JsonParser {
-
-    try {
+    public static void main(String[] args) {
         JsonFactory jsonFactory = new JsonFactory();
-        JsonParser parser = jsonFactory.createParser(new File("Person.json"));
+        com.fasterxml.jackson.core.JsonParser parser;
+        try {
+            parser = jsonFactory.createParser(new File("Person.json"));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
 
-        while (parser.nextToken() != JsonToken.END_OBJECT) {
-            String token = parser.getCurrentName();
+        while (true) {
+            try {
+                if (parser.nextToken() == JsonToken.END_OBJECT) break;
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            String token;
+            try {
+                token = parser.getCurrentName();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
 
             if ("firstname".equals(token)) {
-                parser.nextToken();
+                try {
+                    parser.nextToken();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
 
-                String fname = parser.getText();
+                String fname;
+                try {
+                    fname = parser.getText();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
                 System.out.println("firstname : " + fname);
             }
-                // ...
-            }
-            parser.clone();
-        } catch(IOException e) {
-            // Handle exceptions
-        e.printStackTrace();
+            // ...
+        }
     }
 }
