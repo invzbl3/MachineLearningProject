c730053724b92ea759cb0e8dd9880c4a75276e28
Maksym Rohachov <romax982@gmail.com>
2024-01-04 01:03:00 +0100

    Update the business-logic

---
+++ src/main/java/one/chapter/MultiLayer.java
@@ -0,0 +1,58 @@
+package one.chapter;
+
+public class MultiLayer {
+    public static void main(String[] args) {
+
+        MultiLayerConfiguration conf = new
+                NeuralNetConfiguration.Builder()
+                    .seed(seed)
+                    .iterations(numberOfIterations)
+                    .optimizationAlgo(
+                OptimizationAlgorithm.LINE_GRADIENT_DESCENT
+                    )
+                    .list()
+                    .layer(0, new RBM.Builder()
+                       .nIn(numberOfRows *
+                               numberOfColumns).nOut(1000)
+                            .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                            .build())
+                    .layer(1, new
+                            RBM.Builder().nln(1000).nOut(500)
+                                .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                                    .build())
+                            .layer(3, new
+                                    RBM.Builder().nIn(1000).nOut(500)
+                                        .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                                            .build())
+                            .layer(2, new
+                    RBM.Builder().nIn(250).nOut(100)
+                                    .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                                    .build())
+                            .layer(4, new
+                                    RBM.Builder().nIn(100).nOut(30)
+                                        .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                                    .build)) // encoding stops
+                            .layer(5, new
+                RBM.Builder().nIn(30).nOut(100)
+                            .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                            .build()) // decoding starts
+                    .layer(6, new
+                            RBM.Builder().nIn(100).nOut(250)
+                            .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                            .build())
+                .layer(7, new
+                        RBM.Builder().nIn(250).nOut(500)
+                        .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                        .build())
+                .layer(8, new
+                        RBM.Builder().nIn(500).nOut(1000)
+                        .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                        .build())
+                .layer(9, new OutputLayer.Builder(
+                        LossFunctions.LossFunction.RMSE_XENT).nIn(1000)
+                        .nOut(numberOfRows *
+                                numberOfColumns).build())
+                .pretrain(true).backprop(true)
+                .build();
+    }
+}
