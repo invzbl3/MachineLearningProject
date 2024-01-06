af58b7a7829534231d0d47f8d319602f179437f4
Maksym Rohachov <romax982@gmail.com>
2024-01-06 22:50:41 +0100

    Update the logic for the project

---
+++ src/main/java/two/chapter/OverviewOfDatabases.java
@@ -0,0 +1,31 @@
+package two.chapter;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class OverviewOfDatabases {
+
+    /**
+     * CREATE TABLE IF NOT EXISTS `URLTABLE`
+     * (
+     *      `RecordID` INT(11) NOT NULL
+     *      AUTO_INCREMENT,
+     *      `URL` text NOT NULL,
+     *      PRIMARY KEY (`RecordID`(
+     *  );
+     *
+     */
+
+    /*try {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        String url =
+                "jdbc:mysql://localhost:3306/example";
+
+        connection =
+                DriverManager.getConnection(url, "user ID",
+                        "password");
+    } catch (SQLException | ClassNotFoundException ex) {
+        // Handle exceptions
+    }*/
+}
