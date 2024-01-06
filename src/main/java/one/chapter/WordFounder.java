9c80be4fbd0974db4bb6845c1e3fedaa801581da
Maksym Rohachov <romax982@gmail.com>
2024-01-06 04:55:28 +0100

    Update the logic for the project

--- src/main/java/one/chapter/WordFounder.java
+++ src/main/java/one/chapter/WordFounder.java
@@ -12,5 +12,22 @@ public class WordFounder {
                 System.out.println(result);
     } catch (TesseractException e) {
         System.err.println(e.getMessage());
-    }*/
+    }
+
+    ============================================
+    A part of the output is shown next:
+
+    OCR engines have been developed into
+    many lunds oiobiectorlented OCR
+    applications, such as receipt OCR,
+    involve OCR, check OCR, legal billing
+    document OCR
+
+    They can be used for
+
+    - Data entry for business documents,
+    e.g. check, passport, involve, bank
+    statement and receipt
+    - Automatic number plate recognnlon
+    */
 }
