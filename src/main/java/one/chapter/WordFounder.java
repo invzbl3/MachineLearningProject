811e2c1a36edfa0a36b9a8c38a55a2e7b278e648
Maksym Rohachov <romax982@gmail.com>
2024-01-06 04:51:31 +0100

    Update the logic for the project

---
+++ src/main/java/one/chapter/WordFounder.java
@@ -0,0 +1,16 @@
+package one.chapter;
+
+public class WordFounder {
+
+    /*ITesseract instance = new
+            Tesseract();
+
+    try {
+        String result =
+                instance.doOCR(new
+                        File("OCRExample.png"));
+                System.out.println(result);
+    } catch (TesseractException e) {
+        System.err.println(e.getMessage());
+    }*/
+}
