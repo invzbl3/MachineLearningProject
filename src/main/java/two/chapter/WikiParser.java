e1b9f663fc95a658ed1250aa72329aac85267ea5
Maksym Rohachov <romax982@gmail.com>
2024-01-07 13:06:36 +0100

    Change to HttpsURLConnection instead of HttpURLConnection

--- src/main/java/two/chapter/WikiParser.java
+++ src/main/java/two/chapter/WikiParser.java
@@ -1,2 +1,24 @@
-package two.chapter;public class WikiParser {
+package two.chapter;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+public class WikiParser {
+
+    public static void main(String[] args) {
+        try {
+            URL url = new URL(
+                    "https://en.wikipedia.org/wiki/Data_science");
+            HttpsURLConnection connection
+                    = (HttpsURLConnection)
+                    url.openConnection();
+            //...
+        } catch (MalformedURLException ex) {
+            // Handle exceptions
+        } catch (IOException ex) {
+            // Handle exceptions
+        }
+    }
 }
