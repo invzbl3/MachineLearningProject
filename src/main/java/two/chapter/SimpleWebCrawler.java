bee95259d55994491b22faa1c387f40c57fa8819
Maksym Rohachov <romax982@gmail.com>
2024-01-11 18:13:31 +0100

    Update content

--- src/main/java/two/chapter/SimpleWebCrawler.java
+++ src/main/java/two/chapter/SimpleWebCrawler.java
@@ -2,6 +2,42 @@ package two.chapter;
 
 import java.util.ArrayList;
 
+/**
+ * The instance variables are detailed here:
+ *
+ * Variable:
+ * topic
+ *
+ * Use
+ * The keyword that needs to
+ * be in a page for the page to
+ * be accepted
+ *
+ * startingURL
+ *
+ * Use
+ * The URL of the first page
+ *
+ * urlLimiter
+ *
+ * Use
+ * A string that must be
+ * contained in a link before it
+ * will be followed
+ *
+ * pageLimit
+ *
+ * Use
+ * The maximum number of
+ * pages to retrieve
+ *
+ * visitedList
+ *
+ * Use
+ * The ArrayList containing
+ * pages that have already been
+ * visited
+ */
 public class SimpleWebCrawler {
 
     private String topic;
