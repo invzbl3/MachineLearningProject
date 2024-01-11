ae9f23aeb581324c34303fb0cc41e979e558d612
Maksym Rohachov <romax982@gmail.com>
2024-01-11 18:25:56 +0100

    Update content

---
+++ src/main/java/two/chapter/SimpleWebCrawlerTwo.java
@@ -0,0 +1,12 @@
+package two.chapter;
+
+/*public class SimpleWebCrawlerTwo {
+
+    startingURL = https://
+    en.wikipedia.org/wiki/Bishop_Rock, "
+        + "Isles_of_Scilly";
+    urlLimiter = "Bishop_of_Scilly";
+    topic = "shipping route";
+
+    visitPage(startingURL);
+}*/
