a69c250cdf428d14093dfe6a4740111de42fd3f6
Maksym Rohachov <romax982@gmail.com>
2024-01-22 19:57:29 +0100

    Update files for the project

---
+++ src/main/java/two/chapter/PageFetcher.java
@@ -0,0 +1,19 @@
+package two.chapter;
+
+public class PageFetcher {
+
+    /**
+       PageFetcher pageFetcher = new PageFetcher(config);
+       RobotstxtConfig robotstxtConfig =
+     new RobotstxtConfig();
+       RobotstxtServer robotstxtServer =
+     new RobotstxtServer(robotstxtConfig,
+     pageFetcher);
+        CrawlController controller =
+     new CrawlController(config, pageFetcher, robotstxtConfig);
+
+     controller.addSeed(
+        "https://en.wikipedia.org/wiki/
+     Bishop_Rock, _Isles_of_Scilly");
+    **/
+}
