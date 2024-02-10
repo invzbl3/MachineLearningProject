40a06535f61e301f4ac2425a85171bbad7979d24
Maksym Rohachov <romax982@gmail.com>
2024-02-10 22:09:58 +0100

    Update interface PhotosInterface

---
+++ src/main/java/two/chapter/PhotosInterface.java
@@ -0,0 +1,42 @@
+package two.chapter;
+
+public interface PhotosInterface {
+
+    /*PhotosInterface pi = new PhotosInterface(apiKey, secret,
+            new REST());
+
+       We get the first Photo instance from the previous
+       list and then its getUrl to get the image's URL.
+       The PhotosInterface class's getImage
+       method returns a BufferedImage object
+       representing the image as shown here:
+
+       Photo currentPhoto = list.get(0);
+       BufferedImage bufferedImage =
+       pi.getImage(currentPhoto.getUrl());
+
+       The image is then saved to a file using the
+       ImageIO class:
+
+       File outputfile = new
+       File("image.jpg");
+         ImageIO.write(bufferedImage,
+         "jpg", outputfile);
+
+        The getImage method is overloaded. Here, the
+        Photo instance and the size of the image desired
+        are used as arguments to get the BufferedImage
+        instance:
+
+        bufferedImage =
+        pi.getImage(currentPhoto, Size.SMALL);
+
+        The image can be saved to a file using the previous
+        technique.
+
+        The Flickr4Java API supports a number of other
+        techniques for working with Flickr images.
+    */
+
+
+}
