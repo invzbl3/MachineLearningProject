1c24c153366c49d69a38071ae082235bb4ff2487
Maksym Rohachov <romax982@gmail.com>
2024-02-24 23:03:59 +0100

    Update class PDDocument, chapter #3

---
+++ src/main/java/three/chapter/PDDocument.java
@@ -0,0 +1,40 @@
+package three.chapter;
+
+import java.io.File;
+
+public class PDDocument {
+
+    /*try {
+        PDDocument document =
+                PDDocument.load(new File("PDF File.pdf"));
+        ...
+    } catch (Exception e) {
+        // Handle exceptions
+    }
+
+    Once loaded, the PDFTextStripper class
+    getText method will extract the text from the
+    file. The text is then displayed as shown here:
+
+    PDFTextStripper Tstripper = new
+    PDFTextStripper();
+
+    String documentText =
+    Tstripper.getText(document);
+    System.out.println(documentText);
+
+    The output of this example follows. Notice that the
+    bullets are returned as question marks.
+
+    This is a simple PDF file. It
+    consists of several bullets:
+
+    ? Line 1
+    ? Line 2
+    ? Line 3
+    This is the end of the document.
+
+    This is a brief introduction to the use of PDFBox.
+    It is a very powerful tool when we need to extract and otherwise manipulate PDF
+    documents.*/
+}
