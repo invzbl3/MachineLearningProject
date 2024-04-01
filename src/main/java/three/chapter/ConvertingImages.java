92b9812c73f761d411ba669cdc606d59a58c4407
Maksym Rohachov <romax982@gmail.com>
2024-04-01 04:45:44 +0200

    Add ConvertingImages class to the chapter #3

---
+++ src/main/java/three/chapter/ConvertingImages.java
@@ -0,0 +1,27 @@
+/*
+package three.chapter;
+
+public class ConvertingImages {
+
+    */
+/*Mat source =
+            Imgcodecs.imread("cat.jpg");
+
+    Imgcodecs.imwrite("convertedCat.jpg",
+            source);
+    Imgcodecs.imwrite("convertedCat.jpeg",
+            source);
+    Imgcodecs.imwrite("convertedCat.webp",
+            source);
+    Imgcodecs.imwrite("convertedCat.png",
+            source);
+    Imgcodecs.imwrite("convertedCat.tiff",
+            source);*//*
+
+    /**
+     * The images can now be used for specialized
+     * processing if necessary.
+     *//*
+
+}
+*/
